d93c2a47c55620a7d1db2a778f764f273e9759f0
malbarr <malbar@iau.edu.sa>
2026-04-17 18:47:56 +0300

    Refactor content rendering in fix_rotation.py
    Refactor the way content is rendered in the rot-content-body element, improving handling of special characters and structure.

--- fix_rotation.py
+++ fix_rotation.py
@@ -5,8 +5,10 @@ c = c.replace(
     '${(item.items||[]).length} sections'
 )
 
-old = "document.getElementById('rot-content-body').innerHTML = renderWikiContent(item.content || '');"
-new = "var h='';(item.items||[]).forEach(function(s){h+='<div class=\"rot-section\"><div class=\"rot-section-title\">'+s.subtitle+'</div><pre class=\"rot-content\">'+s.content+'</pre></div>';});document.getElementById('rot-content-body').innerHTML=h;"
+old = "var h='';(item.items||[]).forEach(function(s){h+='<div class=\"rot-section\"><div class=\"rot-section-title\">'+s.subtitle+'</div><pre class=\"rot-content\">'+s.content+'</pre></div>';});document.getElementById('rot-content-body').innerHTML=h;"
+
+new = "var h='';(item.items||[]).forEach(function(s){var lines=s.content.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').split('\\n');var body=lines.map(function(l){return l?'<div class=\"rot-line\">'+l+'</div>':'<div class=\"rot-gap\"></div>';}).join('');h+='<div class=\"rot-section\"><div class=\"rot-section-title\">'+s.subtitle+'</div><div class=\"rot-body\">'+body+'</div></div>';});document.getElementById('rot-content-body').innerHTML=h;"
+
 c = c.replace(old, new)
 
 open("webapp.js", "w").write(c)
